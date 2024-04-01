ad55ccfdfc713b3749555838e391b3d15f711ca8
Your Name <you@example.com>
2024-04-01 23:00:05 +0200

    add Post

--- Code Road/Services/PostService/IPostService.cs
+++ Code Road/Services/PostService/IPostService.cs
@@ -1,6 +1,21 @@
-﻿namespace Code_Road.Services.PostService
+﻿using Code_Road.Dto.Post;
+using Code_Road.Dto.Account;
+
+namespace Code_Road.Services.PostService
 {
     public interface IPostService
     {
+        public Task<List<PostDto>> GetAllAsync();
+        public Task<List<PostDto>> GetAllByUserIdAsync(string user_id);
+        public Task<PostDto> GetByIdAsync(int post_id);
+        public Task<PostDto> AddPostAsync(AddPostDto postModel);
+        public Task<PostDto> UpdatePostAsync(int post_id, UpdatePostDto postModel);
+        public Task<StateDto> DeletePostAsync(int post_id);
+
+        Task<StateDto> IncreaseUpvoteAsync(int postId);
+        Task<StateDto> IncreaseDownvoteAsync(int postId);
+        Task<StateDto> DecreaseUpvoteAsync(int postId);
+        Task<StateDto> DecreaseDownvoteAsync(int postId);
+
     }
 }
