bf66c9fbfa6c31b8852e604eb76ed1d984a9128c
amr3ita <amrwalied90@gmail.com>
2024-04-02 17:47:47 +0200

    add migration

---
+++ Code Road/Migrations/20240401214417_Update Database.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Code_Road.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Image_UserId",
+                table: "Image");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Image_UserId",
+                table: "Image",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Image_UserId",
+                table: "Image");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Image_UserId",
+                table: "Image",
+                column: "UserId",
+                unique: true,
+                filter: "[UserId] IS NOT NULL");
+        }
+    }
+}
