b8e5252f6d22e1cb937cbad18da5df014437c3c9
kemosaid <kemo01094209943@gmail.com>
2024-07-04 00:05:28 +0300

    User Profile v2

--- Code Road/Dto/User/UserProfileDto.cs
+++ Code Road/Dto/User/UserProfileDto.cs
@@ -5,6 +5,6 @@ namespace Code_Road.Dto.User
     public class UserProfileDto
     {
         public UsersReactDto? UserInfo { get; set; } = new UsersReactDto();
-        public List<PostDto>? Posts { get; set; } = new List<PostDto>();
+        public List<PostAndCommentsDto>? Posts { get; set; } = new List<PostAndCommentsDto>();
     }
 }
